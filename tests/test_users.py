7a4c4f81e848397947bde7f1544b39f9dcad5168
Syed Sarim <hashmi.sarim16@gmail.com>
2025-07-30 17:19:26 +0500

    Validation token added

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,6 +1,8 @@
 import pytest
 from app import schemas
 from .database import client,session 
+from jose import jwt
+from app.config import settings
 
 @pytest.fixture
 def test_user(client):
@@ -8,16 +10,15 @@ def test_user(client):
                  "password": "password123"}
     res = client.post("/users/", json=user_data)
     assert res.status_code == 201
-    print(res.json())
     new_user = res.json()
     new_user['password'] = user_data['password']
     return new_user
 
-def test_root(client):
-    res = client.get("/")
-    print(res.json().get('message'))
-    assert(res.json().get('message'))=='Hello World'
-    assert(res.status_code)==200
+#def test_root(client):
+    #res = client.get("/")
+    #print(res.json().get('message'))
+    #assert(res.json().get('message'))=='Hello World'
+    #assert(res.status_code)==200
 
 def test_create_user(client):
     res = client.post("/users/", json={"email": "hello@gmail.com", "password": "password123"})
@@ -25,7 +26,13 @@ def test_create_user(client):
     assert new_user.email== "hello@gmail.com"
     assert res.status_code == 201
 
-def test_login_user(test_user,client):
+def test_login_user(test_user, client):
     res = client.post(
         "/login", data={"username": test_user['email'], "password": test_user['password']})
+    login_res = schemas.Token(**res.json())
+    payload = jwt.decode(login_res.access_token,
+                        settings.secret_key, algorithms=[settings.algorithm])
+    id = payload.get("user_id")
+    assert id == test_user['id']
+    assert login_res.token_type == "bearer"
     assert res.status_code == 200
