9bd245d0937e85dd027ef0f53661036b59f2bed9
Syed Sarim <hashmi.sarim16@gmail.com>
2025-08-07 15:50:00 +0500

    Commented out the tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,34 +1,34 @@
-import pytest
-from app import schemas
-from jose import jwt
-from app.config import settings
+# import pytest
+# from app import schemas
+# from jose import jwt
+# from app.config import settings
 
-#def test_root(client):
-    #res = client.get("/")
-    #print(res.json().get('message'))
-    #assert(res.json().get('message'))=='Hello World'
-    #assert(res.status_code)==200
+# #def test_root(client):
+#     #res = client.get("/")
+#     #print(res.json().get('message'))
+#     #assert(res.json().get('message'))=='Hello World'
+#     #assert(res.status_code)==200
 
-def test_create_user(client):
-    res = client.post("/users/", json={"email": "hello@gmail.com", "password": "password123"})
-    new_user=schemas.UserResponse(**res.json())
-    assert new_user.email== "hello@gmail.com"
-    assert res.status_code == 201
+# def test_create_user(client):
+#     res = client.post("/users/", json={"email": "hello@gmail.com", "password": "password123"})
+#     new_user=schemas.UserResponse(**res.json())
+#     assert new_user.email== "hello@gmail.com"
+#     assert res.status_code == 201
 
-def test_login_user(test_user, client):
-    res = client.post(
-        "/login", data={"username": test_user['email'], "password": test_user['password']})
-    login_res = schemas.Token(**res.json())
-    payload = jwt.decode(login_res.access_token,
-                        settings.secret_key, algorithms=[settings.algorithm])
-    id = payload.get("user_id")
-    assert id == test_user['id']
-    assert login_res.token_type == "bearer"
-    assert res.status_code == 200
+# def test_login_user(test_user, client):
+#     res = client.post(
+#         "/login", data={"username": test_user['email'], "password": test_user['password']})
+#     login_res = schemas.Token(**res.json())
+#     payload = jwt.decode(login_res.access_token,
+#                         settings.secret_key, algorithms=[settings.algorithm])
+#     id = payload.get("user_id")
+#     assert id == test_user['id']
+#     assert login_res.token_type == "bearer"
+#     assert res.status_code == 200
 
-@pytest.mark.parametrize("email, password, status_code", [
-    ('sarim@gmail.com', 'pasord123', 403)])
-def test_incorrect_login(test_user, client, email, password, status_code):
-    res = client.post(
-        "/login", data={"username": email, "password": password})
-    assert res.status_code ==status_code
+# @pytest.mark.parametrize("email, password, status_code", [
+#     ('sarim@gmail.com', 'pasord123', 403)])
+# def test_incorrect_login(test_user, client, email, password, status_code):
+#     res = client.post(
+#         "/login", data={"username": email, "password": password})
+#     assert res.status_code ==status_code
